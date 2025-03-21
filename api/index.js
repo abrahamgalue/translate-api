6b8341d5c4751896a57a23bed5bf2aa1ad27a353
Abraham Galue <abrahamgalue512@gmail.com>
2025-03-21 12:27:44 -0400

    Fixed issue with static file paths

--- api/index.js
+++ api/index.js
@@ -1,15 +1,17 @@
 const express = require('express')
+const path = require('path')
+const cors = require('cors')
+
+// const { corsMiddleware } = require('./middlewares/cors')
 const { createRouter } = require('./routes/translate')
 const { TranslateModel } = require('./models/Gemini/translate')
-// const { corsMiddleware } = require('./middlewares/cors')
-const cors = require('cors')
 
 const app = express()
 app.disable('x-powered-by')
 app.use(cors())
 
-app.get('/', (req, res) => res.sendFile(process.cwd() + '/api/static/index.html'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'static', 'index.html')))
 app.use('/api', createRouter({ translateModel: TranslateModel }))
-app.use((req, res, next) => res.status(404).sendFile(process.cwd() + '/api/static/404.html'))
+app.use((req, res, next) => res.status(404).sendFile(path.join(__dirname, 'static', '404.html')))
 
 module.exports = app
--- api/routes/translate.js
+++ api/routes/translate.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const path = require('path')
 const { TranslateController } = require('../controllers/translate')
 
 export function createRouter({ translateModel }) {
@@ -6,7 +7,7 @@ export function createRouter({ translateModel }) {
 
   const translateController = new TranslateController({ translateModel })
 
-  translateRouter.get('/', (req, res) => res.sendFile(process.cwd() + '/api/static/index.html'))
+  translateRouter.get('/', (req, res) => res.sendFile(path.join(__dirname, '..', 'static', 'index.html')))
   translateRouter.get('/translate', translateController.translateText)
 
   return translateRouter
